54b6ca754cd87624c199b649913581d565c9d2ba
Iris Mirski <158137553+spinelessinvertabrate@users.noreply.github.com>
2024-02-06 16:56:01 -0600

    Add files via upload

---
+++ octogame.hyperesources/triviaInfo.js
@@ -0,0 +1,79 @@
+function getInfo() {
+	window.numberOfQuestions = 	10;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"OCTOPUS TRIVIA";
+	window.questions = [		"HOW MANY HEARTS DO OCTOPUS HAVE?", 
+								"WHAT CLASS OF ANIMAL DO OCTOPUS BELONG TO?",
+								"THEIR FAVORITE FOOD IS...", 
+								"OCTOPUS USE _______ TO CAMOFLAUGE FROM ENEMIES AND HUNT PREY.", 
+								"OCTOPUS HAVE A LEVEL OF INTELLIGENCE EQUIVILANT TO...",
+								"OCTOPUS HAVE AN AVERADGE LIFESPAN OF _ YEARS.",
+								"OCTOPUS LAY HOW MANY EGGS?",
+								"OCTOPUS HAVE ____ - LIKE TOUNGES",
+								"OCTOPUS CAN DO THESE THINGS WITH THEIR SKIN...",
+								"OCTOPUS HAVE OCTO IN THEIR NAME BECAUSE..."];
+	window.correctAnswer = [ 	"C", "C", "E", "D", "A", "C", "E","C","B","B"]; 
+	window.answerA = [ 			"7", 
+								"INSECT", 
+								"FRUITS, VEGTABLES", 
+								"MAGIC", 
+								"A 3 YEAR OLD HUMAN",
+								"1-2 MONTHS",
+								"1.000",
+								"SNAIL-LIKE",
+								"SMELL AND TASTE",
+								"THEY HAVE 8 EYES"];
+	window.answerB = [ 			"1", 
+								"AMPHIBIANS", 
+								"SEA URCHINS, SEA SLUGS", 				
+								"METAMORPHISIS",
+								"A CHIPMUNK", 
+								"7-19 YEARS",
+								"THEY DONT LAY EGGS",
+								"BUG-LIKE",
+								"BREATHE AND SEE",
+								"THEY HAVE 8 TENTICLES"];
+	window.answerC = [ 			"3",
+								"CEPHOLOPADA",						
+								"SEAWEED", 						
+								"SCIENTOLOGY", 
+								"A SPIDER",
+								"1-5 YEARS",
+								"1",
+								"CAT-LIKE",
+								"EAT WITH IT",
+								"OCTO MEANS 6 AND THEY HAVE 6 SUCTION CUPS"];
+	window.answerD = [ 			"4",  
+								"ARACHNID",
+								"WHALES AND SHARKS", 
+								"METACHROSIS",
+								"A JELLYFISH",
+								"12 YEARS",
+								"100",
+								"BIRD-LIKE",
+								"HEAR WITH IT",
+								"OCTO MEANS THEY ARE SMARTER THAN OTHER ANIMALS"];
+	window.answerE = [ 			"0",
+								"MAMMALS", 
+								"SHRIMP, LOBSTER AND CRAB", 
+								"PHOTOSYNTHESIS", 
+								"A CLOWNFISH",
+								"10-50 DAYS",
+								"100,000 to 500,000",
+								"COW-LIKE",
+								"MAKE INK WITH IT",
+								"THEY HAVE 8 BRAINS"];
+	window.response = [ 		"OCTOPUS HAVE 3 HEARTS.", 
+								"OCTOPUS BELONG TO CEPHOLOPADA.", 
+								"SHRIMP, LOBSTER, CRABS.", 
+								"OCTOPUS USE A PROCESS CALLED PHYSICAL COLOR CHANGE OTHERWISE KNOWN AS METACHROSIS TO CAMOFLAUGE.", 
+								"OCTOPUS ARE EXTREMELY INTELLIGENT AND CAN BE AS INTELLIGENT AS A 3 YEAR OLD HUMAN.",
+								"1-5 YEARS.",
+								" 100,000 to 500,000",
+								"OCTOPUS HAVE CAT-LIKE TOUNGES",
+								"OCTOPUS SEE AND BREATHE WITH THEIR SKIN",
+								"OCTO MEANS 8 AND OCTOPUS HAVE 8 LEGS"];
+							
+}
+						
